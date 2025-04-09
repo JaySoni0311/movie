ba1793d0692c88b29a18a7551e77f53bef71174f
jays_2712 <jaysoni.js2712@gmail.com>
2025-04-09 12:21:29 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
+
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client"; // Correct import
 import { HashRouter } from "react-router-dom";
 import "./css/index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>
+const root = createRoot(document.getElementById('root')); // Use createRoot directly
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
