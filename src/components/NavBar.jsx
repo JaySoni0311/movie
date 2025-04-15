4cdd38466458175dcd54151fba1ef24e39741661
jays_2712 <jaysoni.js2712@gmail.com>
2025-04-15 12:03:22 +0530

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import "../css/Navbar.css"
+import "../css/Navbar.css";
 import logo from "../images/logo.jpg";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function NavBar() {
-    return <nav className="navbar">
-        <div className="navbar-brand">
-            <Link to="/"> <img src={logo} alt="logo" /> </Link>
+  const [isOpen, setIsOpen] = useState(false);
 
-        </div>
-        <div className="navbar-links">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/favorites" className="nav-link">Favorites</Link>
-        </div>
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
+      </div>
+      <div className={`nav-links ${isOpen ? "open" : ""}`}>
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/favorites" className="nav-link">Favorites</Link>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <GiHamburgerMenu size={40} color="white" />
+      </div>
     </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
